bdd7210aec73509ae08fdc694461c969df943591
Nathan Grant <Nathan Grant@NathanGrant-PC.home>
2016-04-05 21:57:43 -0400

    first commit

---
+++ Bus Project/src/application/view/controller/PrinterController.java
@@ -0,0 +1,11 @@
+package application.view.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class PrinterController 
+{
+	@FXML 
+	public Label customerLabel;
+	
+}
